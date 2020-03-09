b08ff5214b36d0400fb6ae5d9a02829d9dafafb3
Carriane Linzer <carrianelinzer@icloud.com>
2020-03-09 22:30:38 +0000

    lists and notes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <List />
+        <Note />
+      </div>
+    );
+  }
 }
 
 export default App;
